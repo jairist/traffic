d34ddceb14a4dd49fc93b24fbc4ce4e5bed01739
Jairis <jairist@gmail.com>
2017-08-22 19:32:36 -0400

    Crear clase para trabajar con pagina TodoSqa

---
+++ src/com/traffic/todosqa/TodoSqa.java
@@ -0,0 +1,14 @@
+package com.traffic.todosqa;
+
+public class TodoSqa {
+
+	public TodoSqa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
